703e9d11b5dd17d84aa49a1e8346805c6c237752
Taowd <taowd@outlook.com>
2019-03-17 22:50:11 +0800

    修改lombok的报错问题

--- microservice-getway/microservice-getway-zuul-aggregation/src/main/java/com/taowd/pojo/User.java
+++ microservice-getway/microservice-getway-zuul-aggregation/src/main/java/com/taowd/pojo/User.java
@@ -1,20 +1,37 @@
 package com.taowd.pojo;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 /**
  * @author Taowd
  * @date 2018/3/29 - 13:59
  * @Description
  */
-@Setter
-@Getter
-@ToString
 public class User {
 
-    private Integer id;
-    private String name;
-    private Integer age;
+	private Integer id;
+	private String name;
+	private Integer age;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 }
