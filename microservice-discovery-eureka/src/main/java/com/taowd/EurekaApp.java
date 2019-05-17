4f01d73ba6af9499c5acbb2265828cbf0a51d35b
Taowd <taowd@outlook.com>
2019-05-17 21:53:51 +0800

    增加注释信息

--- microservice-discovery-eureka/src/main/java/com/taowd/EurekaApp.java
+++ microservice-discovery-eureka/src/main/java/com/taowd/EurekaApp.java
@@ -4,11 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Eureka服务注册中心
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApp.class, args);
-	}
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Hello");
+        System.out.println(list);
+
+
+        SpringApplication.run(EurekaApp.class, args);
+    }
 }
