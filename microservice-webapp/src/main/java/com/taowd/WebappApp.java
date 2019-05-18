4dbc0d8ed555bf91a99536326d93241bb1ccdf45
Taowd <taowd@outlook.com>
2019-05-18 15:43:04 +0800

    1、新增前端项目实现前后端分离

---
+++ microservice-webapp/src/main/java/com/taowd/WebappApp.java
@@ -0,0 +1,17 @@
+package com.taowd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+// 增加Eureka客户端功能
+@EnableEurekaClient
+public class WebappApp {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WebappApp.class, args);
+	}
+
+}
